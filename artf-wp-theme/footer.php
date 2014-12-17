a4ffdaa6daeef6ea8723e0086f2567dbfed06e42
IsaacLean <isaaclean@creatibyte.com>
2014-12-17 00:19:09 -0800

    Reorganized repo, started WP theme

---
+++ artf-wp-theme/footer.php
@@ -0,0 +1,7 @@
+	<div>
+		<p>And this is the footer.</p>
+	</div>
+	<?php wp_footer(); //Crucial footer hook! ?>
+
+</body>
+</html>
---
+++ artf-wp-theme/header.php
@@ -0,0 +1,12 @@
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+</head>
+<body>
+	<div>
+		<p>Header test.</p>
+	</div>
