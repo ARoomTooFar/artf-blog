d65a4ae3b69212d9d6e5992860d3cce2beddc040
IsaacLean <isaaclean@creatibyte.com>
2014-12-18 07:39:41 -0800

    Imported landing page into WP theme

--- artf-wp-theme/footer.php
+++ artf-wp-theme/footer.php
@@ -1,7 +1,6 @@
-	<div>
-		<p>And this is the footer.</p>
+	<div id="footer">
+		Background banner art found from Google Images... in the future we could be using your art instead!
 	</div>
-	<?php wp_footer(); //Crucial footer hook! ?>
-
+	<?php wp_footer(); ?>
 </body>
 </html>
--- artf-wp-theme/index.php
+++ artf-wp-theme/index.php
@@ -1,32 +1,59 @@
 <?php get_header(); ?>
-
-<div class="content">
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="post-header">
-					<div class="date"><?php the_time( 'M j y' ); ?></div>
-					<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-					<div class="author"><?php the_author(); ?></div>
+<div id="bgwrap">
+	<div class="content">
+		<div id="content">
+    		<h1>What is A Room Too Far?</h1>
+    		<p><b>A Room Too Far</b> is a cooperative dungeon crawler with a persistent user-generated world on an arcade machine. Dungeons are created on a companion website and are then downloaded onto an arcade machine where players can interact with an ever expanding world unique to the each machine. Players explore a <b>cyberpunk</b> world overwhelmed by the alarming growth of a <b>jungle</b>.</p>
+            <p>We are inspired by games such as Gauntlet, Zelda, Dungeon Keeper, and Diablo.</p>
+            <p>A Room Too Far is being developed through the Computer Game Design program at University of California, Santa Cruz. </p>
+			<h1>The story so far...</h1>
+    		<p>There was once a civilization at the dawn of a new age. Their potential for progress was bright. One day everything changed. A jungle began to erupt, spreading across the planet, engulfing and consuming cities at a fantastic rate. They scrambled to build upwards, out of the reach of the rampant growth, into the skies.</p>
+			<p>The safety of the clouds was not to last. Day by day the vicious vegetation chased them upwards until they could no longer build any further. Out of desperation they raised their cities and the surrounding lands into the sky. The very surface of the planet was torn asunder.</p>
+			<p>Again they were safe, but only for a time...</p>
+			<p>The jungle grew upward, surpassing their wildest expectation. One by one the brambles and branches began to pull their grand cities back down to the earth. In a final frenzy of desperation they did the only thing they could do, burn it all to the ground. The shining metal husks of their towers were only thing left standing amongst the ashes. Their precious ground freed.</p>
+			<p>The cycle continues to this day. A cycle of ascension, reconstruction, survival, descent, and immulation. Adventurers delve into the ruins of their forsaken cities to gain power and resources for themselves and their civilization. The world keeps turning, forever changed, as they search for their salvation. A salvation that is always a room too far.</p>
+			<h1>We need you!</h1>
+			<p>This game is ambitious and we will need many talented artists to execute this game to its fullest potential.</p>
+			<p>Right now we are looking for:</p>
+			<ul>
+				<li>An <b>Art Director</b> who can help unify and guide the art direction for the game.</li>
+				<li>A <b>Sound Director</b> who can unify and guide the sound direction for the game.</li>
+				<li><b>3D Artists</b> with 3D modeling, animation, texturing, or lighting skills.</li>
+				<li><b>Concept Artists</b> for environmental and character concept development.</li>
+				<li><b>Graphic Designers</b> for the website and in-game user interfaces.</li>
+				<li><b>Musicians</b> who can create music and enhance the atmosphere of the game.</li>
+				<li><b>Sound Engineers</b> who can produce sound effects for the game.</li>
+			</ul>
+			<p>Students can earn credits through independent study or Art 118 while working on our project!</p>
+			<h1>Interested?</h1>
+			<p>For more information please contact us at: <b>contact@aroomtoofar.com</b></p>
+		</div>
+		<!--<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="post-header">
+						<div class="date"><?php the_time( 'M j y' ); ?></div>
+						<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<div class="author"><?php the_author(); ?></div>
+					</div>
+					<div class="entry clear">
+						<?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+						<?php the_content(); ?>
+						<?php edit_post_link(); ?>
+						<?php wp_link_pages(); ?>
+					</div>
+					<footer class="post-footer">
+						<div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
+					</footer>
 				</div>
-				<div class="entry clear">
-					<?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
-					<?php the_content(); ?>
-					<?php edit_post_link(); ?>
-					<?php wp_link_pages(); ?>
-				</div>
-				<footer class="post-footer">
-					<div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
-				</footer>
-			</div>
-		<?php endwhile; ?>
-			<nav class="navigation index">
-				<div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
-				<div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
-			</nav>
-		<?php else : ?>
-	<?php endif; ?>
+			<?php endwhile; ?>
+				<nav class="navigation index">
+					<div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
+					<div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
+				</nav>
+			<?php else : ?>
+		<?php endif; ?>-->
+	</div>
 </div>
-
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
