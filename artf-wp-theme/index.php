20a1a55c9c8d37213f83ae2783e9aae635d67b97
IsaacLean <isaaclean@creatibyte.com>
2014-12-19 09:19:09 -0800

    Added genericons

--- artf-wp-theme/index.php
+++ artf-wp-theme/index.php
@@ -5,13 +5,9 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="post-header">
-					<h1>
-						<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-							<?php the_title(); ?>
-						</a>
-					</h1>
-					<div class="date"><?php the_time( 'M j y' ); ?></div>
+					<h1><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 					<div class="author"><?php the_author(); ?></div>
+					<div class="date"><?php the_time( 'M j y' ); ?></div>
 				</div>
 				<div class="entry clear">
 					<?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
@@ -22,11 +18,12 @@
 				<div class="post-footer">
 					<div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
 				</div>
+				<hr>
 			</div>
 			<?php endwhile; ?>
 				<div class="navigation index">
-					<div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
-					<div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
+					<div class="alignleft"><?php next_posts_link( '&laquo; Older Entries' ); ?></div>
+					<div class="alignright"><?php previous_posts_link( 'Newer Entries &raquo;' ); ?></div>
 				</div>
 			<?php else : ?>
 			<?php endif; ?>
