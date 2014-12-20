aabc45e499fa534f329ba5af98d8fc976e310f6b
Isaac Lean <isaaclean@creatibyte.com>
2014-12-20 04:23:52 -0800

    Styled comments link and h1

--- artf-wp-theme/index.php
+++ artf-wp-theme/index.php
@@ -6,7 +6,7 @@
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="post-header">
 					<h1><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-					<div class="post-detail">
+					<div class="post-info">
 						<span class="genericon author"><?php the_author(); ?></span>
 						<span class="genericon date"><?php the_time( 'F j, Y' ); ?></span>
 					</div>
@@ -17,8 +17,7 @@
 					
 				</div>
 				<div class="post-footer">
-					<?php edit_post_link(); ?>
-					<span class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></span>
+					<?php comments_popup_link( '0 Comments', '1 Comment', '% Comments', 'genericon comment' ); ?>
 				</div>
 				<hr>
 			</div>
