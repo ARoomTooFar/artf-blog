229d8819a3a2f38b29f1126c4af6efee72196543
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-04 18:15:18 -0800

    Styled navbar

--- artf-wp-theme/header.php
+++ artf-wp-theme/header.php
@@ -5,27 +5,29 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title(''); ?></title>
 	<link href="<?php echo get_template_directory_uri(); ?>/import/bootstrap/css/bootstrap.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy|Roboto|Roboto+Condensed' rel='stylesheet' type='text/css'>
 	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 </head>
 <body>
-	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<nav class="navbar navbar-inverse navbar-fixed-top navbar-style" role="navigation">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">A Room Too Far</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="/">About</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
+        <div class="col-xs-12">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a id="logo" class="navbar-brand" href="/">A Room Too Far</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="/">About</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
