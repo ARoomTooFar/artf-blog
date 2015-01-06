8d7284157d2ca6cb1fbbd6df47373a5329882ba6
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-06 02:02:24 -0800

    Got layout for page-landing.php done

--- artf-wp-theme/page-landing.php
+++ artf-wp-theme/page-landing.php
@@ -4,28 +4,15 @@ Template Name: Landing Template
 */
 ?>
 <?php get_header(); ?>
-	<div id="wrapper">
-		<div id="content">
+	<div class="container">
+		<div id="content" class="col-xs-12">
 			<?php while ( have_posts() ) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="post-header">
-					<h1><a href="<?php the_permalink(); ?>" title="Permanent Link to '<?php the_title_attribute(); ?>'"><?php the_title(); ?></a></h1>
-					<div class="post-info">
-						<span class="genericon author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="Get more posts by <?php the_author_meta( 'display_name' ); ?>"><?php the_author_meta( 'display_name' ); ?></a></span>
-						<span class="genericon date"><?php the_time( 'F j, Y' ); ?></span>
-					</div>
-				</div>
 				<div class="entry clear">
-					<?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
 					<?php the_content(); ?>
-					
 				</div>
-				<hr>
 			</div>
 			<?php endwhile; ?>
-			<p>page landing template loaded</p>
-			<?php comments_template(); ?> 
 		</div>
 		<?php get_footer(); ?>
 	</div>
-	
