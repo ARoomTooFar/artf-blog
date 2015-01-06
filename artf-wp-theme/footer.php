4635fec1330170104b9542a8a738b68ce7e46065
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-06 01:41:32 -0800

    Styled footer

--- artf-wp-theme/footer.php
+++ artf-wp-theme/footer.php
@@ -1,5 +1,5 @@
-	<div class="col-xs-12">
-		Background banner art found from Google Images... in the future we could be using your art instead!
+	<div id="footer" class="col-xs-12">
+		<p class="text-center">Background banner art found from Google Images... in the future we could be using your art instead!</p>
 	</div>
 	<script src="<?php echo get_template_directory_uri(); ?>/import/jquery/jquery-1.11.2.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/import/bootstrap/js/bootstrap.js"></script>
